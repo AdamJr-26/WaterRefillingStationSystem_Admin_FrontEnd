e88c898bc6e6e8f286a8331eb2874d6f1a5bc3f8
Adam <adammarcaida06261996@gmail.com>
2023-02-28 15:35:36 +0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
 
   plugins: [react()],
 });
